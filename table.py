8fbfb933fd689b9666e4fbf408547af507d36597
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-11-02 16:53:46 +0000

    First version of the table program

--- table.py
+++ table.py
@@ -1,5 +1,8 @@
 import sys
-from utils import R
+from utils import R, seedGen, keyGen
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
 
 if len(sys.argv) != 4:
 	print("Missing args! Please run the application with the following arguments:")
@@ -11,9 +14,42 @@ print("to do")
 
 #Variables
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!'#[a-zA-Z0-9?!]
-rainbowFileName = sys.argv[4]
-k = 
-s = 
-l = 
+alphabetLen = len(alphabet)
+rainbowFileName = sys.argv[3]
+l = int(sys.argv[1])
+s = int(sys.argv[2])
+numRows = int((16*(2**s))/(l*2)) 
+k = int((alphabetLen**l)/numRows)+1
 
+print("Output file: {}".format(rainbowFileName))
+print("Num rows: {}".format(numRows))
+print("Password length: {}".format(l))
+print("s value: {}".format(s))
+print("k value: {}".format(k))
+
+padder = padding.PKCS7(algorithms.AES.block_size).padder()
 #Generate RAINBOW TABLE
+usedSeeds = []
+lastPwds = []
+for row in range(0,numRows):
+	while True:
+		seed = seedGen(alphabet, l)
+		if seed not in usedSeeds:
+			usedSeeds.append(seed)
+			break
+
+	pwd = seed
+	for ite in range(0, k):
+		key = bytes(keyGen(pwd), 'UTF-8')
+		cipher = Cipher(algorithms.AES(key), modes.ECB(), default_backend())
+		encryptor = cipher.encryptor()
+		h = encryptor.update(padder.update(key))
+		pwd = R(ite, h, l, alphabet)
+	lastPwds.append(pwd)
+
+#This for is not time consuming
+file = open(rainbowFileName, 'wb')
+for seed, pwd in zip(usedSeeds, lastPwds):
+	file.write(bytes(seed, 'UTF-8'))
+	file.write(bytes(pwd, 'UTF-8'))
+file.close()
--- utils.py
+++ utils.py
@@ -1,12 +1,26 @@
-def R(ite, hashX, passLen, alphabetLen): 
+import random
+
+def R(ite, hashX, passLen, alphabet): 
 	newHashX = (int.from_bytes(hashX, 'big') + ite)#.to_bytes(len(hashX), 'big')
-	msg = []
+	msg = ""
 	for x in range(0,passLen):
 		shift = int(x*((len(hashX)*8)/passLen))
 		value = newHashX >> shift
-		value = value % alphabetLen
-		msg.append(value)
+		value = value % len(alphabet)
+		msg+= alphabet[value]
 	return msg
+
+def keyGen(pwd):
+	res = pwd
+	while len(res) < 16:
+		res += pwd
+	return res[:16]
+
+def seedGen(alphabet, passLen):
+	seed = ""
+	for x in range(0,passLen):
+		seed += alphabet[random.randint(0,len(alphabet)-1)]
+	return seed
 
 def genLine(S1, Sk, passLen, alphabetLen):
 	#convert S1 and Sk into a compressed representation to store in the file
