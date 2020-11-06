dbece0e69232b4e6ba6dc0d6733bfae481ac9404
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-11-06 00:55:14 +0000

    Removed old hashtable

--- table.c
+++ table.c
@@ -80,7 +80,6 @@ int main(int argc, char *argv[])
 		printf("<split processing (optional)>\n");
 		exit(0);
 	}
-	HashTable *table;
 	FILE *fp;
 	h = EVP_aes_128_ecb();
 	ctx = EVP_CIPHER_CTX_new();
