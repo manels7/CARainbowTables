e081d483c32ececf0252f0db0b3854c768d8992d
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-11-06 02:13:39 +0000

    Created tool to concat the rainbow tables when generated in parts

---
+++ rbtConcat.py
@@ -0,0 +1,55 @@
+import glob
+import sys
+
+if len(sys.argv) != 3:
+	print("Missing args! Please run the application with the following arguments:")
+	print("python3 {} <rainbow table location> <pattern>".format(sys.argv[0]))
+	exit()
+
+#Args
+rbtLocation = sys.argv[1]
+rbtPattern = sys.argv[2]
+
+rbtFiles = []
+for file in glob.glob('{}/{}*'.format(rbtLocation,rbtPattern)):
+	rbtFiles.append(file)
+
+FIRST_READ = True
+fp = open('{}/{}concat'.format(rbtLocation,rbtPattern), 'wb')
+
+l = None
+k = b''
+for fileR in rbtFiles:
+	with open(fileR, 'rb') as file:
+		print(fileR)
+		if FIRST_READ:
+			l = int(file.read(1))
+			while True:
+				byte = file.read(1)
+				if byte == b"\n":
+					break
+				k += byte
+			k = int(k)
+			fp.write(bytes(str(l), 'UTF-8'))
+			fp.write(bytes(str(k), 'UTF-8'))
+			fp.write(bytes("\n", 'UTF-8'))
+			FIRST_READ = False
+
+		initialPwd = b''
+		lastPwd = b''
+		while True:
+			byte = file.read(1)
+			if len(initialPwd) < l:
+				initialPwd += byte
+			elif len(lastPwd) < l:
+				lastPwd += byte
+
+			if len(lastPwd) == l:
+				fp.write(initialPwd)
+				fp.write(lastPwd)
+				initialPwd = b''
+				lastPwd = b''
+
+			if byte == b"":
+				break
+fp.close()
