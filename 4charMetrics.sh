30d8ddc96f7e748038254740f1fc5886fd1da3d9
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2020-11-08 03:21:38 +0000

    Add rainbow tables

---
+++ 4charMetrics.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+# 4-character hashs
+# f8340c836d41f77cd92708bbd5443cbe
+# a5da39d04c817287740f53e6bdc13b5c
+# 5321abb3b57f535e0e3186b7a3204dff
+# 626e33f13574402935a30b2d200ebe53
+# ff293b3c17785c6eafcc1d0f0615f445
+# 212549071c902ae28bc239ce6f1eec49
+# c1fdbe6dc8eb0ed6aeea0e877ba836dd
+# 727578e6768a6750ada716f8dbb0e47c
+# c77e40f417dc4ed074b1df3b916f85c9
+# 11cd649a72075e28384bce23c72c253d
+
+echo -e "\t4_11\t\t4_12\t\t4_13\t\t4_14\t\t4_15" >> matrixChar4
+echo -e "f8340c836d41f77cd92708bbd5443cbe\t\c" >> matrixChar4
+./guess rainbowTables/4_11 f8340c836d41f77cd92708bbd5443cbe 1 >> matrixChar4
+./guess rainbowTables/4_12 f8340c836d41f77cd92708bbd5443cbe 1 >> matrixChar4 
+./guess rainbowTables/4_13 f8340c836d41f77cd92708bbd5443cbe 1 >> matrixChar4 
+./guess rainbowTables/4_14 f8340c836d41f77cd92708bbd5443cbe 1 >> matrixChar4 
+./guess rainbowTables/4_15 f8340c836d41f77cd92708bbd5443cbe 1 >> matrixChar4  
+echo "" >> matrixChar4
+echo -e "a5da39d04c817287740f53e6bdc13b5c\t\c" >> matrixChar4
+./guess rainbowTables/4_11 a5da39d04c817287740f53e6bdc13b5c 1 >> matrixChar4
+./guess rainbowTables/4_12 a5da39d04c817287740f53e6bdc13b5c 1 >> matrixChar4
+./guess rainbowTables/4_13 a5da39d04c817287740f53e6bdc13b5c 1 >> matrixChar4
+./guess rainbowTables/4_14 a5da39d04c817287740f53e6bdc13b5c 1 >> matrixChar4
+./guess rainbowTables/4_15 a5da39d04c817287740f53e6bdc13b5c 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "5321abb3b57f535e0e3186b7a3204dff\t\c" >> matrixChar4
+./guess rainbowTables/4_11 5321abb3b57f535e0e3186b7a3204dff 1 >> matrixChar4
+./guess rainbowTables/4_12 5321abb3b57f535e0e3186b7a3204dff 1 >> matrixChar4
+./guess rainbowTables/4_13 5321abb3b57f535e0e3186b7a3204dff 1 >> matrixChar4
+./guess rainbowTables/4_14 5321abb3b57f535e0e3186b7a3204dff 1 >> matrixChar4
+./guess rainbowTables/4_15 5321abb3b57f535e0e3186b7a3204dff 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "626e33f13574402935a30b2d200ebe53\t\c" >> matrixChar4
+./guess rainbowTables/4_11 626e33f13574402935a30b2d200ebe53 1 >> matrixChar4
+./guess rainbowTables/4_12 626e33f13574402935a30b2d200ebe53 1 >> matrixChar4
+./guess rainbowTables/4_13 626e33f13574402935a30b2d200ebe53 1 >> matrixChar4
+./guess rainbowTables/4_14 626e33f13574402935a30b2d200ebe53 1 >> matrixChar4
+./guess rainbowTables/4_15 626e33f13574402935a30b2d200ebe53 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "ff293b3c17785c6eafcc1d0f0615f445\t\c" >> matrixChar4
+./guess rainbowTables/4_11 ff293b3c17785c6eafcc1d0f0615f445 1 >> matrixChar4
+./guess rainbowTables/4_12 ff293b3c17785c6eafcc1d0f0615f445 1 >> matrixChar4
+./guess rainbowTables/4_13 ff293b3c17785c6eafcc1d0f0615f445 1 >> matrixChar4
+./guess rainbowTables/4_14 ff293b3c17785c6eafcc1d0f0615f445 1 >> matrixChar4
+./guess rainbowTables/4_15 ff293b3c17785c6eafcc1d0f0615f445 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "212549071c902ae28bc239ce6f1eec49\t\c" >> matrixChar4
+./guess rainbowTables/4_11 212549071c902ae28bc239ce6f1eec49 1 >> matrixChar4
+./guess rainbowTables/4_12 212549071c902ae28bc239ce6f1eec49 1 >> matrixChar4
+./guess rainbowTables/4_13 212549071c902ae28bc239ce6f1eec49 1 >> matrixChar4
+./guess rainbowTables/4_14 212549071c902ae28bc239ce6f1eec49 1 >> matrixChar4
+./guess rainbowTables/4_15 212549071c902ae28bc239ce6f1eec49 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "c1fdbe6dc8eb0ed6aeea0e877ba836dd\t\c" >> matrixChar4
+./guess rainbowTables/4_11 c1fdbe6dc8eb0ed6aeea0e877ba836dd 1 >> matrixChar4
+./guess rainbowTables/4_12 c1fdbe6dc8eb0ed6aeea0e877ba836dd 1 >> matrixChar4
+./guess rainbowTables/4_13 c1fdbe6dc8eb0ed6aeea0e877ba836dd 1 >> matrixChar4
+./guess rainbowTables/4_14 c1fdbe6dc8eb0ed6aeea0e877ba836dd 1 >> matrixChar4
+./guess rainbowTables/4_15 c1fdbe6dc8eb0ed6aeea0e877ba836dd 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "727578e6768a6750ada716f8dbb0e47c\t\c" >> matrixChar4
+./guess rainbowTables/4_11 727578e6768a6750ada716f8dbb0e47c 1 >> matrixChar4
+./guess rainbowTables/4_12 727578e6768a6750ada716f8dbb0e47c 1 >> matrixChar4
+./guess rainbowTables/4_13 727578e6768a6750ada716f8dbb0e47c 1 >> matrixChar4
+./guess rainbowTables/4_14 727578e6768a6750ada716f8dbb0e47c 1 >> matrixChar4
+./guess rainbowTables/4_15 727578e6768a6750ada716f8dbb0e47c 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "c77e40f417dc4ed074b1df3b916f85c9\t\c" >> matrixChar4
+./guess rainbowTables/4_11 c77e40f417dc4ed074b1df3b916f85c9 1 >> matrixChar4
+./guess rainbowTables/4_12 c77e40f417dc4ed074b1df3b916f85c9 1 >> matrixChar4
+./guess rainbowTables/4_13 c77e40f417dc4ed074b1df3b916f85c9 1 >> matrixChar4
+./guess rainbowTables/4_14 c77e40f417dc4ed074b1df3b916f85c9 1 >> matrixChar4
+./guess rainbowTables/4_15 c77e40f417dc4ed074b1df3b916f85c9 1 >> matrixChar4
+echo "" >> matrixChar4
+echo -e "11cd649a72075e28384bce23c72c253d\t\c" >> matrixChar4
+./guess rainbowTables/4_11 11cd649a72075e28384bce23c72c253d 1 >> matrixChar4
+./guess rainbowTables/4_12 11cd649a72075e28384bce23c72c253d 1 >> matrixChar4
+./guess rainbowTables/4_13 11cd649a72075e28384bce23c72c253d 1 >> matrixChar4
+./guess rainbowTables/4_14 11cd649a72075e28384bce23c72c253d 1 >> matrixChar4
+./guess rainbowTables/4_15 11cd649a72075e28384bce23c72c253d 1 >> matrixChar4
+echo "" >> matrixChar4
